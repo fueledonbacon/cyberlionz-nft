53065fb0a0628bd29073680e273bba6a92c1b44a
Ryan Cwynar <ryan@cwynar.dev>
2022-03-04 00:12:45 -0500

    change game.vue js

--- pages/game.vue
+++ pages/game.vue
@@ -1,139 +1,139 @@
 <template>
-    
-    <div id="unity-container" class="unity-desktop">
+	<div
+		id="unity-container"
+		:class="{
+			'unity-desktop': !unityMobile,
+			'unity-mobile': unityMobile,
+		}">
+		<div class="header">
+			<a href="#default" class="logo"
+				><img src="/cyberlionz-twitimg.gif" height="32px" />
+			</a>
+			<div class="header-right">
+				<a class="active" href="#home">Game</a>
+				<a href="https://twitter.com/CyberLionzNFT" target="_blank">Twitter</a>
+			</div>
+		</div>
 
-			<div class="header">
-				<a href="#default" class="logo"><img src="TemplateData/cyberLionzLogoStatic.png" height="32px"> </a>
-  <div class="header-right">
-    <a class="active" href="#home">Game</a>
-    <a href="https://twitter.com/CyberLionzNFT" target="_blank">Twitter</a>
-  </div>
-</div>
-
-      <canvas id="unity-canvas" width="960" height="600"></canvas>
-      <div id="unity-loading-bar">
-        <div id="unity-logo"></div>
-        <div id="unity-progress-bar-empty">
-          <div id="unity-progress-bar-full"></div>
-        </div>
-      </div>
-      <div id="unity-warning"> </div>
-      <div id="unity-footer">
-        <div id="unity-webgl-logo"></div>
-        <div id="unity-fullscreen-button"></div>
-        <div id="unity-build-title">CyberLionz</div>
-      </div>
-    </div>
-    
+		<canvas
+			id="unity-canvas"
+      ref="unity"
+			:width="canvasWidth"
+			:height="canvasHeight"
+			style="background: url(/Build/webgl.jpg) center / cover"></canvas>
+		<div id="unity-loading-bar" class="block">
+			<div id="unity-logo"></div>
+			<div id="unity-progress-bar-empty" v-if="showLoadingBar">
+				<div
+					id="unity-progress-bar-full"
+					:style="{ width: progressBarPercent }"></div>
+			</div>
+		</div>
+		<div
+			id="unity-warning"
+			:class="`p-4 rounded-md ${bannerColor}`"
+			v-if="showUnityBanner">
+			{{ bannerMsg }}
+		</div>
+		<div id="unity-footer">
+			<div id="unity-webgl-logo"></div>
+			<div id="unity-fullscreen-button" @click.native="setFullscreen"></div>
+			<div id="unity-build-title">CyberLionz</div>
+		</div>
+	</div>
 </template>
 
 <script>
+export default {
+	data() {
+		return {
+			showUnityBanner: false,
+			bannerMsg: '',
+			bannerColor: '',
+			unityMobile: false,
+			canvasWidth: 960,
+			canvasHeight: 600,
+			progressBarPercent: '0%',
+			unityInstance: {},
+		}
+	},
+	mounted() {
 
-this.mounted(){
+		// Shows a temporary message banner/ribbon for a few seconds, or
+		// a permanent error message on top of the canvas if type=='error'.
+		// If type=='warning', a yellow highlight color is used.
+		// Modify or remove this function to customize the visually presented
+		// way that non-critical warnings and error messages are presented to the
+		// user.
 
-      var container = document.querySelector("#unity-container");
-      var canvas = document.querySelector("#unity-canvas");
-      var loadingBar = document.querySelector("#unity-loading-bar");
-      var progressBarFull = document.querySelector("#unity-progress-bar-full");
-      var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-      var warningBanner = document.querySelector("#unity-warning");
+		var config = {
+			dataUrl: '/Build/webgl.data.gz',
+			frameworkUrl: '/Build/webgl.framework.js.gz',
+			codeUrl: '/Build/webgl.wasm.gz',
+			streamingAssetsUrl: 'StreamingAssets',
+			companyName: 'Fueled on Bacon',
+			productName: 'fob-cyberlionz-runner',
+			productVersion: '0.1.4',
+			showBanner: this.unityShowBanner,
+			showLoadingBar: true,
+		}
 
-      // Shows a temporary message banner/ribbon for a few seconds, or
-      // a permanent error message on top of the canvas if type=='error'.
-      // If type=='warning', a yellow highlight color is used.
-      // Modify or remove this function to customize the visually presented
-      // way that non-critical warnings and error messages are presented to the
-      // user.
-      function unityShowBanner(msg, type) {
-        function updateBannerVisibility() {
-          warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
-        }
-        var div = document.createElement('div');
-        div.innerHTML = msg;
-        warningBanner.appendChild(div);
-        if (type == 'error') div.style = 'background: red; padding: 10px;';
-        else {
-          if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
-          setTimeout(function() {
-            warningBanner.removeChild(div);
-            updateBannerVisibility();
-          }, 5000);
-        }
-        updateBannerVisibility();
-      }
+		// By default Unity keeps WebGL canvas render target size matched with
+		// the DOM size of the canvas element (scaled by window.devicePixelRatio)
+		// Set this to false if you want to decouple this synchronization from
+		// happening inside the engine, and you would instead like to size up
+		// the canvas DOM size and WebGL render target sizes yourself.
+		// config.matchWebGLToCanvasSize = false;
 
-      var buildUrl = "Build";
-      var loaderUrl = buildUrl + "/webgl.loader.js";
-      var config = {
-        dataUrl: buildUrl + "/webgl.data.gz",
-        frameworkUrl: buildUrl + "/webgl.framework.js.gz",
-        codeUrl: buildUrl + "/webgl.wasm.gz",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "Fueled on Bacon",
-        productName: "fob-cyberlionz-runner",
-        productVersion: "0.1.4",
-        showBanner: unityShowBanner,
-      };
+		if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+			// Mobile device style: fill the whole browser client area with the game canvas:
 
-      // By default Unity keeps WebGL canvas render target size matched with
-      // the DOM size of the canvas element (scaled by window.devicePixelRatio)
-      // Set this to false if you want to decouple this synchronization from
-      // happening inside the engine, and you would instead like to size up
-      // the canvas DOM size and WebGL render target sizes yourself.
-      // config.matchWebGLToCanvasSize = false;
+			this.unityMobile = true
 
-      if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-        // Mobile device style: fill the whole browser client area with the game canvas:
+			// To lower canvas resolution on mobile devices to gain some
+			// performance, uncomment the following line:
+			config.devicePixelRatio = 1
 
-        var meta = document.createElement('meta');
-        meta.name = 'viewport';
-        meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
-        document.getElementsByTagName('head')[0].appendChild(meta);
-        container.className = "unity-mobile";
+			this.canvasWidth = window.innerWidth
+			this.canvasHeight = window.innerHeight
 
-        // To lower canvas resolution on mobile devices to gain some
-        // performance, uncomment the following line:
-        config.devicePixelRatio = 1;
+			this.unityShowBanner(
+				'CyberLionz WebGL is not fully compatabile with mobile devices - Your experience may vary!'
+			)
+		} else {
+			// Desktop style: Render the game canvas in a window that can be maximized to fullscreen:
+		}
 
-        //canvas.style.width = "960px";
-        //canvas.style.height = "600px";
-        canvas.style.width = window.innerWidth + 'px';
-        canvas.style.height = window.innerHeight + 'px';
-        //canvas.style.width = window.innerWidth + 'px';
-        //canvas.style.height = window.innerWidth / 1.778 + 'px';
-
-        unityShowBanner('CyberLionz WebGL is not fully compatabile with mobile devices - Your experience may vary!');
-      } else {
-        // Desktop style: Render the game canvas in a window that can be maximized to fullscreen:
-
-        canvas.style.width = "960px";
-        canvas.style.height = "600px";
-      }
-
-      canvas.style.background = "url('" + buildUrl + "/Builds.jpg.gz') center / cover";
-      loadingBar.style.display = "block";
-
-      var script = document.createElement("script");
-      script.src = loaderUrl;
-      script.onload = () => {
-        createUnityInstance(canvas, config, (progress) => {
-          progressBarFull.style.width = 100 * progress + "%";
-        }).then((unityInstance) => {
-          loadingBar.style.display = "none";
-          fullscreenButton.onclick = () => {
-            unityInstance.SetFullscreen(1);
-          };
-        }).catch((message) => {
-          alert(message);
-        });
-      };
-      document.body.appendChild(script);
-
+		var script = {}
+		script.onload = () => {
+			window.createUnityInstance(this.$refs.unity, config, (progress) => {
+					this.progressBarPercent = 100 * progress + '%'
+				})
+				.then((unityInstance) => {
+					this.showLoadingBar = false
+					this.unityInstance = unityInstance
+				})
+				.catch((message) => {
+					alert(message)
+				})
+		}
+	},
+	methods: {
+		setFullscreen() {
+			window.unityInstance.SetFullscreen(1)
+		},
+		unityShowBanner(msg, type) {
+			const _this = this
+			this.bannerMsg = msg
+			this.showUnityBanner = true
+			if (type == 'error') this.bannerColor = 'bg-red-400'
+			else {
+				if (type == 'warning') this.bannerColor = 'bg-yellow-400'
+				setTimeout(function () {
+					this.showUnityBanner = false
+				}, 5000)
+			}
+		},
+	},
 }
-
-export default defineComponent({
-    setup() {
-        
-    },
-})
 </script>
