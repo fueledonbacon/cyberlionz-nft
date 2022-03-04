339b879af7d6ea62695a498b784a14875473329d
Kyle LeMaster <kyle.lemaster@gmail.com>
2022-03-04 07:51:01 -0600

    Added htaccess

--- components/Home/About.vue
+++ components/Home/About.vue
@@ -13,15 +13,6 @@
 					flex-col-reverse
 					items-center
 				">
-				
-				<div class="w-full lg:w-1/2 lg:px-0 mb-16 hidden w-96 mb-0 lg:block">
-					<div class="w-full h-full px-20 rounded-2xl ">
-						<img
-							data-aos="fade-in"
-							src="@/assets/images/lion-cub-idle.gif"
-							alt="Lion Cub Artist" />
-					</div>
-				</div>
 
 				<div
 					class="w-full lg:w-1/2 mb-16 ml-0 sm:px-5 sm:pl-8 xl:pl-20 lg:mb-0">
@@ -56,6 +47,17 @@
 						</div>
 					</div>
 				</div>
+				
+				<div class="w-full lg:w-1/2 lg:px-0 mb-16 hidden w-96 mb-0 lg:block">
+					<div class="w-full h-full px-20 rounded-2xl ">
+						<img
+							data-aos="fade-in"
+							src="@/assets/images/lion-cub-idle.gif"
+							alt="Lion Cub Artist" />
+					</div>
+				</div>
+
+				
 			</div>
 		</div>
 	</section>
