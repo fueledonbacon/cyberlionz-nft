9b07515c5d885319c7ce34e06eac85bce04d54b4
Alexis Tanaka <legendlancer0314@gmail.com>
2022-03-23 17:59:10 -0400

    add-carousel-component

---
+++ components/Home/Carousel.vue
@@ -0,0 +1,28 @@
+<template>
+	<section class="relative bg-lionz-light-brown" id="about-section">
+		<carousel-3d
+			:count="images.length"
+			class="py-5"
+			style="margin: 0 !important; height: 100%">
+			<slide v-for="(image, i) in images" :index="i">
+				<img :src="image" />
+			</slide>
+		</carousel-3d>
+	</section>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d'
+export default {
+	components: {
+		Carousel3d,
+		Slide,
+	},
+	computed: {
+		images() {
+			const path = require.context('@/assets/images/carousel', false, /\.gif$/)
+			return path.keys().map(path)
+		},
+	},
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
 	<div id="app" class="min-h-screen w-full bg-gray-200">
 		<main>
-			<HomeHeroMint/>
-			<HomeAbout/>
-			<HomeAbout2/>
-			<Game/>
-			<HomeRoadmap/>
-			<HomeTeam/>
-			<HomeFaq/>
-			<Footer/>
+			<HomeHeroMint />
+			<HomeAbout />
+			<HomeCarousel />
+			<HomeAbout2 />
+			<Game />
+			<HomeRoadmap />
+			<HomeTeam />
+			<HomeFaq />
+			<Footer />
 		</main>
 	</div>
 </template>
 
 <script>
-
 export default {
 	name: 'Home',
 	components: {},
