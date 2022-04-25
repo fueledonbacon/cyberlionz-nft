799d495144fd3ad546faf8242a5da30d3a53b10a
Alexis Tanaka <devalex0313@gmail.com>
2022-04-25 13:48:40 -0400

    staking-updated

---
+++ components/StakingHeader.vue
@@ -0,0 +1,30 @@
+<template>
+	<header class="w-full px-8 text-gray-700 z-50">
+		<div
+			class="
+				container
+				flex flex-col flex-wrap
+				items-center
+				justify-between
+				py-5
+				mx-auto
+				md:flex-row
+				max-w-7xl
+			">
+			<div>
+				<span class="mr-5">Total Items Staked: 10</span>
+				<span>Heat Amount: {{ this.$wallet.heatAmount }}</span>
+			</div>
+			<div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
+				<connected-button />
+				<!-- <span class="text-xl">
+					{{ displayTime }}
+				</span> -->
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {}
+</script>
