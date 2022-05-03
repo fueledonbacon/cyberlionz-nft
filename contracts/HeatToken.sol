f2cad2b169b8569a2632c84388f46504be3a8408
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-05-03 13:49:10 -0500

    WIP functions in heat

--- contracts/HeatToken.sol
+++ contracts/HeatToken.sol
@@ -1,11 +1,32 @@
-pragma solidity ^0.8.11;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract HeatToken is ERC20 {
-    constructor() ERC20("HeatToken", "HT") {}
 
-    function mint(uint256 amount) external {
-        _mint(msg.sender, amount);
+contract HeatToken is ERC20, AccessControl {
+    bytes32 private  _MINTER_ROLE = keccak256("MINTER_ROLE");
+
+
+    constructor(address minter) ERC20("HeatToken", "HT") {
+        _setupRole(_MINTER_ROLE, minter);
+
     }
+
+    function mint(address to, uint256 amount) external {
+        require(hasRole(_MINTER_ROLE, msg.sender), "DOES_NOT_HAVE_MINTER_ROLE");
+        _mint(to, amount);
+
+    }
+
+    function _setApprovalForAll(address owner, address operator) public {
+        _setApprovalForAll(owner, operator);
+    }
+
+   function _burn(uint256 tokenId) external {
+       // Todo write this funcionality
+   }
+
+
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,16 +3,23 @@
  */
  require('dotenv').config();
  require("@nomiclabs/hardhat-waffle");
+ require("@nomiclabs/hardhat-etherscan");
+
  const { 
   ALCHEMY_ROPSTEN_API_KEY,
   ALCHEMY_RINKEBY_API_KEY,
   ALCHEMY_MAINNET_API_KEY,
-  CONTRACT_OWNER_PRIVATE_KEY
+  CONTRACT_OWNER_PRIVATE_KEY,
+  ETHERSCAN_API_KEY
 } = process.env;
  
 module.exports = {
-  solidity: "0.8.7",
+  solidity: "0.8.13",
   defaultNetwork: "localhost",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+
+  },
   networks: {
     localhost: {},
     ropsten: {
@@ -20,8 +27,8 @@ module.exports = {
       accounts: [CONTRACT_OWNER_PRIVATE_KEY]
     },
     rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_RINKEBY_API_KEY}`,
-      accounts: [CONTRACT_OWNER_PRIVATE_KEY]
+      url: 'https://rinkeby.infura.io/v3/294756524c724e35ae8e7cb59f4eca94',
+      accounts: ["33f5a1922e2f346777a5b75e2ef23803c998e6627053fa84da6e237ad180dbb3"]
     },
     mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_MAINNET_API_KEY}`,
