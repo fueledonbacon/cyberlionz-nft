c03eac16400d089c8d1d70856fb570402a7d08dc
Ryan Cwynar <ryan@cwynar.dev>
2022-05-05 12:20:48 -0500

    formatting

--- contracts/HeatToken.sol
+++ contracts/HeatToken.sol
@@ -1,45 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/access/AccessControl.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract HeatToken is ERC20, AccessControl {
-    bytes32 private  _MINTER_ROLE = keccak256("MINTER_ROLE");
+	bytes32 private _MINTER_ROLE = keccak256('MINTER_ROLE');
 
-  function isAdmin(address account) public virtual view returns(bool)
-  {
-    return hasRole(DEFAULT_ADMIN_ROLE, account);
-  }
-  function isMinter(address account) public virtual view returns(bool)
-  {
-    return hasRole(_MINTER_ROLE, account);
-  }
-  modifier onlyAdmin() {
-    require(isAdmin(msg.sender), "Restricted to admins.");
-    _;
-  }
-    modifier onlyMinters() {
-    require(isMinter(msg.sender), "Restricted to minters.");
-    _;
-  }
+	function isAdmin(address account) public view virtual returns (bool) {
+		return hasRole(DEFAULT_ADMIN_ROLE, account);
+	}
 
-    constructor() ERC20("HeatToken", "HT") {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender); 
+	function isMinter(address account) public view virtual returns (bool) {
+		return hasRole(_MINTER_ROLE, account);
+	}
 
-    }
+	modifier onlyAdmin() {
+		require(isAdmin(msg.sender), 'Restricted to admins.');
+		_;
+	}
+	modifier onlyMinters() {
+		require(isMinter(msg.sender), 'Restricted to minters.');
+		_;
+	}
 
-    function setMinterRole(address minter) public onlyAdmin {
-        _setupRole(_MINTER_ROLE, minter);
-    }
+	constructor() ERC20('HeatToken', 'HT') {
+		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+	}
 
-    function mint(address to, uint256 amount) external onlyMinters {
-        _mint(to, amount);
-    }
-    
-    function burn(uint256 _amount) external {
-        _burn(msg.sender, _amount);
-    }
+	function setMinterRole(address minter) public onlyAdmin {
+		_setupRole(_MINTER_ROLE, minter);
+	}
 
+	function mint(address to, uint256 amount) external onlyMinters {
+		_mint(to, amount);
+	}
 
+	function burn(uint256 _amount) external {
+		_burn(msg.sender, _amount);
+	}
 }
