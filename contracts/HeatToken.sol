c5890a10ac4ee98f6a4a62d767a91f39dc5b6f23
Ryan Cwynar <ryan@cwynar.dev>
2022-05-05 09:26:14 -0500

    add roles on HeatToken.sol

--- contracts/HeatToken.sol
+++ contracts/HeatToken.sol
@@ -4,34 +4,28 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-
-
-
 contract HeatToken is ERC20, AccessControl {
-    bytes32 private  _MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 private  _ADMIN_ROLE = keccak256("ADMIN");
+    bytes32 public  MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public  ADMIN_ROLE = keccak256("ADMIN");
 
     constructor() ERC20("HeatToken", "HT") {
-
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(ADMIN_ROLE, msg.sender);
     }
-    function _setRole(address minter) public {
-        _setupRole(_MINTER_ROLE, minter);
-    }
-    
 
-    function mint(address to, uint256 amount) external {
+    function setMinterRole(address minter) public onlyRole(ADMIN_ROLE){
+        _setupRole(MINTER_ROLE, minter);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
-
     }
 
     function _setApprovalForAll(address owner, address operator) public {
         _setApprovalForAll(owner, operator);
     }
     
-   function burn(uint256 _amount) external {
-        _burn(msg.sender, _amount);
-   }
-
-
+    function burn(address from, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _burn(from, amount);
+    }
 }
