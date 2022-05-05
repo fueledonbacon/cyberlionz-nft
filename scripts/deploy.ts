6c5858eef9e670c5c2010e276b526c5fb9c93daf
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-05-05 11:22:44 -0500

    deployment Script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -19,9 +19,9 @@ async function main() {
   console.log("Transaction hash: " + clStaking.deployTransaction.hash);
 
   
-  const deployedContract = await CLStaking.attach(clStaking.address)
+  // const deployedContract = await CLStaking.attach(clStaking.address)
   
-  await deployedContract.setCollection(CYBERLIONZ_NFT_ADDRESS, STAKING_REWARDS)
+  // await deployedContract.setCollection(CYBERLIONZ_NFT_ADDRESS, STAKING_REWARDS)
   
 
 }
