ed968721369813595f335e060fa91a59f7fb376b
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-05-05 15:16:23 -0500

    Heat format

--- components/StakingHeader.vue
+++ components/StakingHeader.vue
@@ -18,12 +18,12 @@
 				>
 				<span class="mr-5"
 					>$HEAT:
-					<span class="text-xl">{{ this.$wallet.heatAmount }}</span></span
+					<span class="text-xl">{{ (this.$wallet.heatAmount)}}</span></span
 				>
 
 					<span
 					>Unclaimed $HEAT:
-					<span class="text-xl">{{ this.$wallet.claimableReward }}</span></span
+					<span class="text-xl">{{ this.$wallet.claimableReward/(10**8).toFixed(3)  }}</span></span
 				>
 			</div>
 			<div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
