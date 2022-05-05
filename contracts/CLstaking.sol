b1241f8056c21c35796d42c9f0e37b0b3de8af7e
Ryan Cwynar <ryan@cwynar.dev>
2022-05-05 09:34:50 -0500

    wip

--- contracts/CLstaking.sol
+++ contracts/CLstaking.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
