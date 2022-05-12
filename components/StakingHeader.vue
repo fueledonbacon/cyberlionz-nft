359936a82b2c1f209eca359bb782c3fe2c6d6b89
Leonardo Malavera G <5674931+leomalaveraG@users.noreply.github.com>
2022-05-12 17:05:26 -0500

    use parseint

--- components/StakingHeader.vue
+++ components/StakingHeader.vue
@@ -18,13 +18,13 @@
 				>
 				<span class="mr-5"
 					>$HEAT:
-					<span class="text-xl">{{ Number(this.$wallet.heatAmount).toFixed(0)}}</span></span
+					<span class="text-xl">{{ parseInt(this.$wallet.heatAmount)}}</span></span
 				>
 
 					<span
 					>Unclaimed $HEAT:
-					<span class="text-xl">{{ Number(this.$wallet.claimableReward).toFixed(0) }}</span></span
-				>
+
+					<span class="text-xl">{{  parseInt(this.$wallet.claimableReward)}}</span></span>
 			</div>
 			<div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
 				<connected-button />
