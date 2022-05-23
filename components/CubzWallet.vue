c8e89cccb5beebc86c4158bc786ce6ddcf34fce3
Alexis Tanaka <devalex0313@gmail.com>
2022-05-23 08:20:53 -0400

    evolving-without-heat

--- components/CubzWallet.vue
+++ components/CubzWallet.vue
@@ -10,7 +10,7 @@
 			bg-cover
 		"
 		id="cubzwallet-section">
-		<StakingHeader />
+		<EvolvingHeader />
 		<div class="px-[5%]">
 			<div
 				class="
@@ -24,7 +24,7 @@
 					items-center
 					mb-3
 				">
-				<button
+				<a
 					class="
 						bg-[url('@/static/Buttons/btn-stake.gif')]
 						hover:bg-[url('@/static/Buttons/btn-stake-hover.gif')]
@@ -32,8 +32,9 @@
 						bg-no-repeat bg-contain bg-center
 						h-[60%]
 						w-[20%]
-					"></button>
-				<button
+					"
+					href="/stake"></a>
+				<a
 					class="
 						bg-[url('@/static/Buttons/btn-evolve.gif')]
 						hover:bg-[url('@/static/Buttons/btn-evolve-hover.gif')]
@@ -41,8 +42,9 @@
 						bg-no-repeat bg-contain bg-center
 						h-[60%]
 						w-[20%]
-					"></button>
-				<button
+					"
+					href="#"></a>
+				<a
 					class="
 						bg-[url('@/static/Buttons/btn-market.gif')]
 						hover:bg-[url('@/static/Buttons/btn-market-hover.gif')]
@@ -50,7 +52,9 @@
 						bg-no-repeat bg-contain bg-center
 						h-[60%]
 						w-[20%]
-					"></button>
+					"
+					href="#"
+					@click="showModal = true"></a>
 			</div>
 			<div
 				class="
@@ -79,9 +83,24 @@
 					@dragenter.prevent></div>
 				<div class="ml-[18%] mt-1 text-gray-300">
 					<span class="text-xl">{{
-						$wallet.nfts === undefined ? 0 : $wallet.nfts.length
+						$wallet.nfts !== undefined && $wallet.nfts.length
 					}}</span>
 					ITEMS
+				</div>
+				<div
+					v-if="
+						$wallet.nfts !== undefined &&
+						$wallet.loaded &&
+						$wallet.nfts.length == 0 &&
+						$wallet.loaded != -1
+					"
+					class="flex justify-center items-center h-[170px]">
+					<p class="text-[#d1d5db]">Inventory is empty</p>
+				</div>
+				<div
+					v-if="$wallet.loaded == -1"
+					class="flex justify-center items-center h-[170px]">
+					<p class="text-[#d1d5db]">Please connect your wallet</p>
 				</div>
 				<div class="flex gap-6 overflow-x-auto hover:scroll-auto custom-scrollbar">
 					<div
@@ -93,7 +112,7 @@
 						:key="i"
 						class="flex-none pt-3">
 						<img
-							:src="'https://ipfs.io/ipfs' + item.image.substring(6)"
+							:src="`${item.image}?${curTime}`"
 							class="w-[140px] h-[140px] hover:cursor-pointer"
 							data-aos="fade-right"
 							@dragstart="startDrag($event, item.id)" />
@@ -107,28 +126,29 @@
 					@dropped="onDrop_1"
 					@exchange="onExchange"
 					ref="slot1"
-					class="absolute left-[11%] top-[17.8%] w-[25.6%] h-[36%]" />
+					class="absolute left-[11%] top-[17.8%] w-[25.6%] h-[36%]"
+					:curTime="curTime" />
 				<trait-slot
 					:index="this.dropId1"
 					:toggleState="toggleValue"
 					class="absolute left-[13.8%] top-[58.5%] w-[18.5%] h-[38.3%]" />
 				<div>
-					<div class="absolute left-[43%] top-[12.8%] w-[14.2%] h-[19.9%]">
+					<div class="absolute left-[43.05%] top-[12.8%] w-[14.3%] h-[20.1%]">
 						<loading
 							:active="previewImageLoading"
 							:width="100"
 							:height="100"
 							color="#f59e0b"
+							background-color="#000000"
 							loader="dots"
-							:opacity="0.6"
+							:opacity="0.7"
 							:is-full-page="false" />
 						<img
-							:src="previewImage"
+							:src="`${this.previewImage}?${curTime}`"
 							data-aos="fade"
-							v-if="previewImage != undefined" />
+							v-if="this.previewImage != undefined" />
 					</div>
 					<button
-						href="#_"
 						class="
 							absolute
 							left-[43.8%]
@@ -139,6 +159,17 @@
 						"
 						:disabled="dropId1 === undefined || dropId2 === undefined"
 						@click="onPreview"></button>
+					<button
+						class="
+							absolute
+							left-[43.8%]
+							top-[42%]
+							w-[12.8%]
+							h-[6.1%]
+							bg-[url('@/static/Evolving/Lions-UI_Evolve_Glowing.gif')]
+							bg-no-repeat bg-contain bg-center
+						"
+						@click="onEvolve"></button>
 					<div class="absolute left-[44.4%] top-[59.3%] w-[11.5%] h-[34.9%]">
 						<div
 							v-for="(val, index) in trait_type"
@@ -159,13 +190,36 @@
 					@dropped="onDrop_2"
 					@exchange="onExchange"
 					ref="slot2"
-					class="absolute left-[63.8%] top-[17.8%] w-[25.6%] h-[36%]" />
+					class="absolute left-[63.8%] top-[17.8%] w-[25.6%] h-[36%]"
+					:curTime="curTime" />
 				<trait-slot
 					:index="this.dropId2"
 					:toggleState="toggleValueR"
 					class="absolute left-[67.6%] top-[58.5%] w-[18.5%] h-[38.3%]" />
 			</div>
 		</div>
+		<loading
+			:active="this.$wallet.evolving != ''"
+			:width="120"
+			:height="120"
+			color="#f59e0b"
+			background-color="#000000"
+			loader="dots"
+			:opacity="0.8"
+			:is-full-page="true"
+			:lock-scroll="true"
+			><p class="text-[#d1d5db]">{{ this.$wallet.evolving }}</p></loading
+		>
+		<notifications group="foo" />
+
+		<VueFinalModal
+			v-model="showModal"
+			classes="modal-container"
+			content-class="modal-content">
+			<div>
+				<p class="text-gray-300">Coming Soon</p>
+			</div>
+		</VueFinalModal>
 	</section>
 </template>
 
@@ -189,12 +243,31 @@
 }
 </style>
 
+<style scoped>
+::v-deep .modal-container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+::v-deep .modal-content {
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	margin: 0 1rem;
+	padding: 1rem;
+	border-color: #2d3748;
+	background-color: #1a202c;
+}
+</style>
+
 <script>
 import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
 import { PerfectScrollbar } from 'vue2-perfect-scrollbar'
 import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css'
+import { VueFinalModal } from 'vue-final-modal'
 import axios from 'axios'
+import Vue from 'vue'
 
 export default {
 	data() {
@@ -219,15 +292,20 @@ export default {
 				Hands: true,
 				Mouth: true,
 				Eyewear: true,
+				Headwear: true,
 				Companion: true,
 			},
 			previewImage: undefined,
 			previewImageLoading: false,
+			filename: '',
+			curTime: new Date().getTime(),
+			showModal: false,
 		}
 	},
 	components: {
 		Loading,
 		PerfectScrollbar,
+		VueFinalModal,
 	},
 	computed: {
 		toggleValueR() {
@@ -257,10 +335,24 @@ export default {
 			}
 		},
 		onDrop_1(index) {
-			this.dropId1 = index
+			if (this.$wallet.nfts[index].name.includes('Adultlion'))
+				Vue.notify({
+					group: 'foo',
+					type: 'error',
+					title: '',
+					text: 'Already evolved.',
+				})
+			else this.dropId1 = index
 		},
 		onDrop_2(index) {
-			this.dropId2 = index
+			if (this.$wallet.nfts[index].name.includes('Adultlion'))
+				Vue.notify({
+					group: 'foo',
+					type: 'error',
+					title: '',
+					text: 'Already evolved.',
+				})
+			else this.dropId2 = index
 		},
 		onExchange() {
 			;[this.dropId1, this.dropId2] = [this.dropId2, this.dropId1]
@@ -274,20 +366,76 @@ export default {
 							index
 						].value)
 			)
+			params.Headwear = 'None'
+			let fn = ''
+			Object.entries(params).forEach(([key, value]) => (fn += value))
+			this.filename = fn
 			try {
 				this.previewImageLoading = true
-				const res = await axios.get(`/.netlify/functions/evolve`, {
-					headers: {
-						'Access-Control-Allow-Origin': '*',
-						'Content-Type': 'application/json',
-					},
-					params,
-				})
-				this.previewImageLoading = false
-				this.previewImage = res.data
+				const response = await fetch(
+					`https://${process.env.s3Bucket}.s3.amazonaws.com/gifs_evolve/${this.filename}.gif`
+				)
+				if (response.status == 200) {
+					this.previewImageLoading = false
+					this.previewImage = `https://${process.env.s3Bucket}.s3.amazonaws.com/gifs_evolve/${this.filename}.gif`
+				} else {
+					await axios.get(`/.netlify/functions/preview-background`, {
+						params,
+					})
+					const intervalId = setInterval(async () => {
+						const response = await fetch(
+							`https://${process.env.s3Bucket}.s3.amazonaws.com/gifs_evolve/${this.filename}.gif`
+						)
+						if (response.status == 200) {
+							clearInterval(intervalId)
+							this.previewImageLoading = false
+							this.previewImage = `https://${process.env.s3Bucket}.s3.amazonaws.com/gifs_evolve/${this.filename}.gif`
+							this.curTime = new Date().getTime()
+						}
+					}, 5000)
+				}
 			} catch (err) {
 				console.log(err)
 			}
+		},
+		async onEvolve() {
+			let traits = {}
+			Object.entries(this.toggleValue).forEach(
+				([key, value], index) =>
+					(traits[key] =
+						this.$wallet.nfts[value ? this.dropId1 : this.dropId2].attributes[
+							index
+						].value)
+			)
+			traits.Headwear = 'None'
+			const new_id = this.$wallet.nfts[this.dropId1].name.split('#')[1]
+			const old_id = this.$wallet.nfts[this.dropId2].name.split('#')[1]
+
+			this.$wallet.evolving = 'evolving...'
+
+			await axios.get(`/.netlify/functions/evolve`, {
+				params: {
+					oldName: this.filename,
+					newName: this.$wallet.nfts[this.dropId1].name.split('#')[1],
+					traits,
+				},
+			})
+
+			this.$wallet.evolving = 'burning...'
+
+			await this.$wallet.burn(parseInt(old_id))
+			;[this.dropId1, this.dropId2, this.previewImage] = [
+				undefined,
+				undefined,
+				undefined,
+			]
+
+			this.$wallet.evolving = 'reloading...'
+			setTimeout(async () => {
+				this.curTime = new Date().getTime()
+				this.$wallet.nfts = await this.$wallet.getNfts(this.$wallet.account)
+				this.$wallet.evolving = ''
+			}, 20000)
 		},
 	},
 }
--- plugins/wallet.js
+++ plugins/wallet.js
@@ -13,7 +13,7 @@ export default async ({ $config, store }, inject) => {
 		cyberLizonAddress,
 		heatContractAbi,
 		heatContractAddress,
-		cubzNetwork
+		cubzNetwork,
 	} = $config.smartContracts
 	const { infuraId, moralisApiKey } = $config.providers
 
@@ -32,6 +32,7 @@ export default async ({ $config, store }, inject) => {
 		claimableReward: 0,
 		loaded: -1,
 		staking: '',
+		evolving: '',
 		Web3Modal: null,
 		async contractState() {},
 
@@ -46,7 +47,6 @@ export default async ({ $config, store }, inject) => {
 		},
 
 		async init() {
-			
 			// skip this and autologin
 			if (!window.ethereum) {
 				window.ethereum = await this.Web3Modal.connect()
@@ -71,10 +71,11 @@ export default async ({ $config, store }, inject) => {
 
 			await this.updateClaimableReward()
 
-			setInterval(async ()=>{
+			setInterval(async () => {
 				await this.updateClaimableReward()
-			}, 30 * 1000 )
+			}, 30 * 1000)
 		},
+
 		async getNfts(newAccount) {
 			const nftContract = new ethers.Contract(
 				cyberLizonAddress,
@@ -99,9 +100,7 @@ export default async ({ $config, store }, inject) => {
 					i = 0
 				for (let nft of res.data.result) {
 					const token_uri = await nftContract.tokenURI(parseInt(nft.token_id))
-					let metadata = await axios.get(
-						'https://ipfs.io/ipfs' + token_uri.substring(6)
-					)
+					let metadata = await axios.get(token_uri)
 					metadata.data.id = i++
 					results.push(metadata.data)
 				}
@@ -119,27 +118,28 @@ export default async ({ $config, store }, inject) => {
 				heatContractAbi,
 				this.provider
 			)
-            try{
-
-				this.heatAmount = ethers.utils.formatEther(await heatContract.balanceOf(this.account))
-			} catch (e){
+			try {
+				this.heatAmount = ethers.utils.formatEther(
+					await heatContract.balanceOf(this.account)
+				)
+			} catch (e) {
 				console.log(e)
 			}
 		},
 
-
-
-		async updateClaimableReward(){
+		async updateClaimableReward() {
 			const stakingContract = new ethers.Contract(
 				clStakinAddress,
 				clStakinABi,
 				this.provider
 			)
 
-			try{
-				console.log();
-				this.claimableReward = ethers.utils.formatEther(await stakingContract.totalClaimableReward(this.account, 0))
-			} catch (e){
+			try {
+				console.log()
+				this.claimableReward = ethers.utils.formatEther(
+					await stakingContract.totalClaimableReward(this.account, 0)
+				)
+			} catch (e) {
 				console.log(e)
 			}
 		},
@@ -152,8 +152,14 @@ export default async ({ $config, store }, inject) => {
 				clStakinABi,
 				this.provider
 			)
-			this.claimableReward = await stakingContract.totalClaimableReward(this.account, 0)
-			this.stakeInfo.userInfo = await stakingContract.getUserStakedTokens(this.account, 0)
+			this.claimableReward = await stakingContract.totalClaimableReward(
+				this.account,
+				0
+			)
+			this.stakeInfo.userInfo = await stakingContract.getUserStakedTokens(
+				this.account,
+				0
+			)
 			this.stakeInfo.total = await stakingContract.getTotalStakedItemsCount(0)
 		},
 
@@ -206,7 +212,7 @@ export default async ({ $config, store }, inject) => {
 
 				await this.getStakeInfo()
 				await this.getHeatInfo()
-				await this.getNfts(this.account)
+				this.nfts = await this.getNfts(this.account)
 
 				Vue.notify({
 					group: 'foo',
@@ -246,20 +252,20 @@ export default async ({ $config, store }, inject) => {
 					clStakinABi,
 					this.provider.getSigner()
 				)
-			
+
 				const tx_unstake = await stakingContract.batchUnstake(0, unstakeItems)
 				this.staking = 'Unstaking...'
 				await tx_unstake.wait()
 
-				unstakeItems.map(function(value, key){
-					this.stakeItems.splice(value,1)
+				unstakeItems.map(function (value, key) {
+					this.stakeItems.splice(value, 1)
 				})
 				this.nfts = []
 				this.loaded = false
 
 				await this.getStakeInfo()
 				await this.getHeatInfo()
-				await this.getNfts(this.account)
+				this.nfts = await this.getNfts(this.account)
 
 				Vue.notify({
 					group: 'foo',
@@ -279,6 +285,26 @@ export default async ({ $config, store }, inject) => {
 				this.staking = ''
 			}
 		},
+
+		async burn(tokenId) {
+			const nftContract = new ethers.Contract(
+				cyberLizonAddress,
+				cyberLizonAbi,
+				this.provider.getSigner()
+			)
+
+			const tx_burn = await nftContract.transferFrom(
+				this.account,
+				'0x000000000000000000000000000000000000dEaD',
+				tokenId,
+				{
+					gasLimit: 250000,
+				}
+			)
+
+			await tx_burn.wait()
+		},
+
 		async setContract() {
 			if (this.network.chainId !== $config.smartContracts.chainId) {
 				await this.switchNetwork($config.smartContracts.chainId)
@@ -287,23 +313,27 @@ export default async ({ $config, store }, inject) => {
 				await this.connect()
 			}
 
-			const contract = new ethers.Contract(cyberLizonAddress, cyberLizonAbi, this.provider.getSigner())
+			const contract = new ethers.Contract(
+				cyberLizonAddress,
+				cyberLizonAbi,
+				this.provider.getSigner()
+			)
 
 			this.contract = contract
-			console.log(`Contected to: ${$config.smartContracts.cyberLizonAddress} Contract`)
+			console.log(
+				`Contected to: ${$config.smartContracts.cyberLizonAddress} Contract`
+			)
 		},
-		async getContract(){
-			if(this.contract)
-			  return this.contract
-			try{
+
+		async getContract() {
+			if (this.contract) return this.contract
+			try {
 				await this.setContract()
 				return this.contract
-			} catch(e) {
+			} catch (e) {
 				console.log(e)
 			}
-
 		},
-
 
 		async setAccount(newAccount) {
 			if (newAccount) {
