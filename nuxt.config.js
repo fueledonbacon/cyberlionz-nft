55fa6074896d2698a8f5ce4042f55bc29bbbf559
Ryan Cwynar <ryan@cwynar.dev>
2022-06-08 17:23:20 -0500

    wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,17 @@ import { abi as cyberLizonAbi } from './artifacts/contracts/CyberLionz.sol/Cyber
 import { abi as clStakinABi } from './artifacts/contracts/CLstaking.sol/CyberLionzStaking.json'
 import { abi as heatContractAbi } from './artifacts/contracts/HeatToken.sol/HeatToken.json'
 
+if (
+	process.env.LD_LIBRARY_PATH == null ||
+	!process.env.LD_LIBRARY_PATH.includes(
+		`${process.env.PWD}/node_modules/canvas/build/Release:`
+	)
+) {
+	process.env.LD_LIBRARY_PATH = `${
+		process.env.PWD
+	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
+}
+
 export default {
 	ssr: false,
 	target: 'static', //Static page generation
