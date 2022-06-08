d1cac6c3ddcf8734e5354a1e080f906a688c3435
Ryan Cwynar <ryan@cwynar.dev>
2022-06-08 17:33:01 -0500

    add stuff

--- netlify/functions/evolve.js
+++ netlify/functions/evolve.js
@@ -1,3 +1,14 @@
+if (
+	process.env.LD_LIBRARY_PATH == null ||
+	!process.env.LD_LIBRARY_PATH.includes(
+		`${process.env.PWD}/node_modules/canvas/build/Release:`
+	)
+) {
+	process.env.LD_LIBRARY_PATH = `${
+		process.env.PWD
+	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
+}
+
 var AWS = require('aws-sdk')
 const fs = require('fs')
 const basePath = process.cwd()
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,16 +2,16 @@ import { abi as cyberLizonAbi } from './artifacts/contracts/CyberLionz.sol/Cyber
 import { abi as clStakinABi } from './artifacts/contracts/CLstaking.sol/CyberLionzStaking.json'
 import { abi as heatContractAbi } from './artifacts/contracts/HeatToken.sol/HeatToken.json'
 
-if (
-	process.env.LD_LIBRARY_PATH == null ||
-	!process.env.LD_LIBRARY_PATH.includes(
-		`${process.env.PWD}/node_modules/canvas/build/Release:`
-	)
-) {
-	process.env.LD_LIBRARY_PATH = `${
-		process.env.PWD
-	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
-}
+// if (
+// 	process.env.LD_LIBRARY_PATH == null ||
+// 	!process.env.LD_LIBRARY_PATH.includes(
+// 		`${process.env.PWD}/node_modules/canvas/build/Release:`
+// 	)
+// ) {
+// 	process.env.LD_LIBRARY_PATH = `${
+// 		process.env.PWD
+// 	}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
+// }
 
 export default {
 	ssr: false,
