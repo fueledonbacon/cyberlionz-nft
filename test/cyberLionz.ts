c13b86f05b2a5cade93c508aa73b91378f74a40e
sdelvalle57 <sdelvalle57@gmail.com>
2022-06-21 11:47:32 -0500

    Successfully tested CyberLionzMerger

--- test/cyberLionz.ts
+++ test/cyberLionz.ts
@@ -37,16 +37,40 @@ describe('CyberlionzMerger', function () {
 
     await cyberlionz.airdrop(signers[1].address, 10);
 
+    await heatToken.setMinterRole(signers[0].address);
     await heatToken.mint(signers[1].address, ethers.utils.parseEther("10000"));
     
   });
 
 
-
   it('Merge cubz', async function () {
-    expect(2).to.equal(2);
+    expect((await cyberlionz.totalSupply()).toNumber()).to.be.equal(10)
+
+    await heatToken.connect(signers[1]).approve(cyberlionzMerger.address, ethers.utils.parseEther("10000"));
+    await cyberlionz.connect(signers[1]).setApprovalForAll(cyberlionzMerger.address, true);
+    await cyberlionzMerger.connect(signers[1]).mergeCubz(1, 2);
+
+    expect((await heatToken.balanceOf(signers[1].address))).to.be.equal(ethers.utils.parseEther("9900"));
+    expect((await heatToken.balanceOf(cyberlionzMerger.address))).to.be.equal(ethers.utils.parseEther("100"));
+
+    expect((await cyberlionz.ownerOf(1)).toLowerCase()).to.be.equal(cyberlionzMerger.address.toLowerCase());
+    expect((await cyberlionz.ownerOf(2)).toLowerCase()).to.be.equal(signers[1].address.toLowerCase());
+
+    //tries to use it again
+    await expect(
+      cyberlionzMerger.connect(signers[1]).mergeCubz(3, 2)
+    ).to.be.revertedWith('Cub reached max');
+
+    const lockedCubz = await cyberlionzMerger.getLockedLionz();
+    expect(lockedCubz.length).to.be.equal(1);
+    expect(lockedCubz[0]).to.be.equal(1);
+
+    expect((await cyberlionzAdults.balanceOf(signers[1].address)).toNumber()).to.be.equal(1);
+    expect((await cyberlionzAdults.ownerOf(1)).toLowerCase()).to.be.equal(signers[1].address.toLowerCase())
+
+    await cyberlionzMerger.withdrawFunds(signers[0].address);
+    expect((await heatToken.balanceOf(cyberlionzMerger.address))).to.be.equal(ethers.utils.parseEther("0"));
+    expect((await heatToken.balanceOf(signers[0].address))).to.be.equal(ethers.utils.parseEther("100"))
+
   });
-
-  
-
 });
